63ffbc0859d5704a9a71bf4edc37dfef0f5e2382
Zac Hayes <zrev2220@gmail.com>
2022-05-06 12:23:24 -0400

    Add tests

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -14,12 +14,10 @@ export const Counter: React.FC<CounterProps> = ({
   let suffix = propSuffix;
   if (pluralString && count !== 1) suffix += pluralString;
 
+  const text = `Count: ${count}${suffix}`;
   return (
     <div>
-      <div>
-        Count: {count}
-        {suffix}
-      </div>
+      <div>{text}</div>
       <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
     </div>
   );
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean;
+}
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
